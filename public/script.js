5cf33f4847fe1f8f391deccaf429462c71e8b031
Mateus Evaristo Melo <mateusevaristom@gmail.com>
2026-05-01 15:03:01 -0300

    Adicionando catálogo JSON

--- public/script.js
+++ public/script.js
@@ -1,1 +1,71 @@
-console.log("Script Funcionando");
+const catalogo = [
+{
+    id: 1,
+    titulo: "As Crônicas de Nárnia",
+    tipo: "filme",
+    ano: 2005,
+    generos: ["fantasia", "aventura"],
+    nota: 8.5,
+    assistido: true
+},
+{
+    id: 2,
+    titulo: "KillBill Part 1",
+    tipo: "filme",
+    ano: 2003,
+    generos: ["ação", "vingança"],
+    nota: 10,
+    assistido: false
+
+},
+{
+    id: 3,
+    titulo: "KillBill Part 2",
+    tipo: "filme",
+    ano: 2004,
+    generos: ["ação", "vingança"],
+    nota: 10,
+    assistido: false
+
+},
+{
+    id: 4,
+    titulo: "Era do Gelo",
+    tipo: "filme",
+    ano: 2002,
+    generos: ["animação", "comédia"],
+    nota: 10,
+    assistido: true
+
+},
+{
+    id: 5,
+    titulo: "Breaking Bad",
+    tipo: "serie",
+    ano: 2008,
+    generos: ["drama", "crime"],
+    nota: 10,
+    assistido: false
+
+},
+{
+    id: 6,
+    titulo: "Taxi Driver",
+    tipo: "filme",
+    ano: 1976,
+    generos: ["drama", "crime"],
+    nota: 10,
+    assistido: true
+
+},
+{
+    id: 7,
+    titulo: "Brilho Eterno de uma Mente sem Lembranças",
+    tipo: "filme",
+    ano: 2004,
+    generos: ["drama", "romance"],
+    nota: 10,
+    assistido: true
+
+},
+];
